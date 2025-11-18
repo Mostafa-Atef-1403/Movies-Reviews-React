b6353b0dd06af183a5752171b98c517687826dfa
Mostafa Atef <moatefelbanby123@gmail.com>
2025-11-18 03:00:52 +0200

    Update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,21 +10,24 @@ import Videos from "./pages/Videos";
 import Images from "./pages/Images";
 import { MovieProvider } from "../context/MovieContext";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 const Main = () => {
   return (
     <MovieProvider>
       <Router>
         <Navbar />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favourite" element={<Favorites />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
           <Route path="/movie/:id/cast" element={<Cast />} />
+
           {/* media */}
           <Route path="/movie/:id/videos" element={<Videos />} />
           <Route path="/movie/:id/images/:type" element={<Images />} />
+
           {/* people */}
           <Route path="/person/:id" element={<Person />} />
         </Routes>
